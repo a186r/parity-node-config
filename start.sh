cbcdc9b612e76198c98de701a7fae0e9ed66fda2
a186r <hi.aewalle@gmail.com>
2019-01-09 11:17:20 +0800

    change node config

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/datadrive/blockchain/parity/parity-ethereum-2.1.10/target/release/parity --config ./node0.toml --jsonrpc-cors '*' &
-/datadrive/blockchain/parity/parity-ethereum-2.1.10/target/release/parity --config ./node1.toml --jsonrpc-cors '*' &
-/datadrive/blockchain/parity/parity-ethereum-2.1.10/target/release/parity --config ./node2.toml --jsonrpc-cors '*' &
-/datadrive/blockchain/parity/parity-ethereum-2.1.10/target/release/parity --config ./node3.toml --jsonrpc-cors '*' &
+/datadrive/blockchain/parity/parity-ethereum-2.1.10/target/release/parity --config ./node0.toml --chain 6652 --jsonrpc-cors '*' &
+/datadrive/blockchain/parity/parity-ethereum-2.1.10/target/release/parity --config ./node1.toml --chain 6652 --jsonrpc-cors '*' &
+/datadrive/blockchain/parity/parity-ethereum-2.1.10/target/release/parity --config ./node2.toml --chain 6652 --jsonrpc-cors '*' &
+/datadrive/blockchain/parity/parity-ethereum-2.1.10/target/release/parity --config ./node3.toml --chain 6652 --jsonrpc-cors '*' &
